2c12627cbf4d21e7eab6290649f6f5b8c453909e
Roman Riezanov <romark6@gmail.com>
2021-08-31 00:45:27 +0300

    First small game on js

--- script.js
+++ script.js
@@ -2,5 +2,33 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-// Write your code below:
+const generateTarget = () => {
+  return Math.floor(Math.random() * 10);
+}
 
+const compareGuesses = (userChoice, computerChoice, target) => {
+
+  const humanDifference = Math.abs(target - userChoice)
+  const computerDifference = Math.abs(target - computerChoice)
+  return humanDifference <= computerDifference;
+  
+  // if (Math.abs(userChoice - target) === Math.abs(computerChoice - target)) {
+  //   return true;
+  // } else if (Math.abs(userChoice - target) > Math.abs(computerChoice - target)) {
+  //   return false
+  // } else {
+  //   return true;
+  // }
+}
+
+const updateScore = (winner) => {
+  if (winner === 'human') {
+    humanScore++
+  } else {
+    computerScore++
+  }
+}
+
+const advanceRound = () => {
+  currentRoundNumber++
+}
